ca5fc90509dcd049b4a2f37d7a66e3ae7bd7291f
Ayushman Gupta <ayushman0021@gmail.com>
2024-08-08 20:38:47 +0530

    chore: Remove unused import in layout.jsx

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,9 +1,7 @@
 import { Inter} from "next/font/google";
-
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
-// const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Awesome Influencer",
@@ -13,10 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
-
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
